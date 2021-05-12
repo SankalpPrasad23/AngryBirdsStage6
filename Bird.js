a0ac4d45b901605b55c36cc5a2442dbcdc39435d
SankalpPrasad23 <prasadsankalp44@gmail.com>
2021-05-12 14:58:20 +0530

    Path Added

--- Bird.js
+++ Bird.js
@@ -2,6 +2,8 @@ class Bird extends BaseClass {
   constructor(x,y){
     super(x,y,50,50);
     this.image = loadImage("sprites/bird.png");
+    this.smoke = loadImage("sprites/smoke.png");
+    this.path = [];
   }
 
   display() {
@@ -9,5 +11,11 @@ class Bird extends BaseClass {
     //this.body.position.y = mouseY;
 
     super.display();
+    if(this.body.velocity.x > 5 && this.body.position.x > 200){
+      this.path.push([this.body.position.x,this.body.position.y])
+    }
+    for(var p = 0;p < this.path.length;p++){
+      image(this.smoke,this.path[p][0],this.path[p][1]);
+    }
   }
 }
